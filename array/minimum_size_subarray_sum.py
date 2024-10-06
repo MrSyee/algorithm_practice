ed4d89059b632cb97fc33bd4a18726510aafb52a
Kyunghwan Kim <khwankim@smilegate.com>
2024-10-06 20:53:38 +0900

    Move sliding window category

--- array/minimum_size_subarray_sum.py
+++ array/minimum_size_subarray_sum.py
@@ -2,10 +2,10 @@
 209. Minimum Size Subarray Sum (Medium)
 https://leetcode.com/problems/minimum-size-subarray-sum/description/?envType=study-plan-v2&envId=top-interview-150
 
-Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+Given an array of positive integers nums and a positive integer target, return the minimal length of a
 subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
- 
+
 Example 1:
 
 Input: target = 7, nums = [2,3,1,2,4,3]
@@ -19,14 +19,14 @@ Example 3:
 
 Input: target = 11, nums = [1,1,1,1,1,1,1,1]
 Output: 0
- 
+
 
 Constraints:
 
 1 <= target <= 109
 1 <= nums.length <= 105
 1 <= nums[i] <= 104
- 
+
 
 Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
 """
--- hash_table/longest_substring_without_repeating_chars.py
+++ hash_table/longest_substring_without_repeating_chars.py
@@ -7,8 +7,8 @@ Given a string, find the length of the longest substring without repeating chara
 Example 1:
 
 Input: "abcabcbb"
-Output: 3 
-Explanation: The answer is "abc", with the length of 3. 
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 Example 2:
 
 Input: "bbbbb"
@@ -18,7 +18,7 @@ Example 3:
 
 Input: "pwwkew"
 Output: 3
-Explanation: The answer is "wke", with the length of 3. 
+Explanation: The answer is "wke", with the length of 3.
 Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 """
 
@@ -59,3 +59,58 @@ class Solution:
                 sol = max(sol, right - left)
             right += 1
         return sol
+
+
+# T_C: O(N * M)
+# S_C: O(1)
+# 305ms, 9.06%
+# 중복된 것이 나왔을 경우 left와 right를 모두 리셋하니 시간이 더 걸린다.
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+
+        length = len(s)
+        left, right = 0, 0
+        ans = 0
+
+        substring = ""
+
+        while left <= right and right < length:
+            if s[right] not in substring:
+                substring += s[right]
+                right += 1
+            else:
+                ans = max(ans, right - left)
+                left += 1
+                right = left
+                substring = ""
+        return max(ans, right - left)
+
+"""
+24.10.06 다시 풀어봄
+"""
+
+# T_C: O(N)
+# S_C: O(1)
+# 38ms, 99.32%
+# left 위치가 중복된 알파벳의 다음으로 이동하도록 해주면 시간을 아낄 수 있다.
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+
+        length = len(s)
+        left, right = 0, 0
+        ans = 0
+
+        char_dict = {}
+
+        for right in range(length):
+            if s[right] not in char_dict or char_dict[s[right]] < left:
+                char_dict[s[right]] = right
+                ans = max(ans, right - left + 1)
+            else:
+                left = char_dict[s[right]] + 1
+                char_dict[s[right]] = right
+        return ans
